5d4c60b3bba0c72e570b16d8d198d2c4372600e4
Matthias <matthias@meidinger.de>
2013-12-16 19:53:02 +0100

    Added documentation to ExtendedFileSink, removed the FID in there, checked the build and run.

--- extendedfilesink.cpp
+++ extendedfilesink.cpp
@@ -1,22 +1,39 @@
 #include "extendedfilesink.h"
 #include <sstream>
 
-ExtendedFileSink::ExtendedFileSink(UsageEnvironment& env, FILE* fid, unsigned bufferSize, std::string tmpCamNameSuffix) : MediaSink(env), fOutFid(fid), fBufferSize(bufferSize) {
+/*!
+ * \brief Protected constructor of the class.
+ * \param env                   UsageEnvironment for usage in MediaSink constructor
+ * \param bufferSize            Unsigned number which defines the buffersize for receiving data
+ * \param tmpCamNameSuffix      Name of the file to save it in the database (on receiving data)
+ */
+ExtendedFileSink::ExtendedFileSink(UsageEnvironment& env, unsigned bufferSize, std::string tmpCamNameSuffix) : MediaSink(env), fBufferSize(bufferSize) {
     fBuffer = new unsigned char[bufferSize];
     this->stringfNameSuffix=tmpCamNameSuffix;
-    std::cout <<"EFS: local ---" << this->stringfNameSuffix<< std::endl;
-    std::cout <<"EFS: global ---" << ExtendedFileSink::stringfNameSuffix<< std::endl;
 }
 
+/*!
+ * \brief Destructor - frees up the space used by the buffer.
+ */
 ExtendedFileSink::~ExtendedFileSink() {
     delete[] fBuffer;
-    //delete[] ExtendedFileSink::fNameSuffix;
 }
 
+/*!
+ * \brief Factory pattern for creating a new instance of this class.
+ * \param env                   UsageEnvironment for usage in MediaSink constructor
+ * \param tmpCamNameSuffix      Name of the file to save it in the database (on receiving data)
+ * \param bufferSize            Unsigned number which defines the buffersize for receiving data
+ * \return                      new Instance of ExtendedFileSink
+ */
 ExtendedFileSink* ExtendedFileSink::createNew(UsageEnvironment& env, std::string tmpCamNameSuffix, unsigned bufferSize) {
-    return new ExtendedFileSink(env, /*fid*/NULL, bufferSize, tmpCamNameSuffix);
+    return new ExtendedFileSink(env, bufferSize, tmpCamNameSuffix);
 }
 
+/*!
+ * \brief Helper function for determining if there is something to play
+ * \return true, if something is there. false, if there is nothing.
+ */
 Boolean ExtendedFileSink::continuePlaying() {
     if (fSource == NULL) return False;
 
@@ -25,11 +42,28 @@ Boolean ExtendedFileSink::continuePlaying() {
     return True;
 }
 
+/*!
+ * \brief Callback funtion for handling an incoming frame.
+ * \param clientData            ExtendedFileSink-Object in which to write
+ * \param frameSize             unsigned number representing the size of the received frame
+ * \param numTruncatedBytes     unsigned number representing the number of truncated bytes (cut off, because frameSize > bufferSize)
+ * \param presentationTime      timeval - since when is the stream running
+ */
 void ExtendedFileSink::afterGettingFrame(void* clientData, unsigned frameSize, unsigned numTruncatedBytes, struct timeval presentationTime, unsigned /*durationInMicroseconds*/) {
     ExtendedFileSink* sink = (ExtendedFileSink*)clientData;
     sink->afterGettingFrame(frameSize, numTruncatedBytes, presentationTime);
 }
 
+/*!
+ * \brief Method for writing given data to the database
+ *
+ * most important / most called function of this class. It uses a mongo::ScopedDbConnection for creating a connection pool.
+ * After creating the pool, it uses mongo::ScopedDbConnection.conn() (a single mongo connection) to create a mongo::GridFS Object.
+ * This is used for writing files to the database grDB.
+ * \param data              The data that has to be written. Usually, it is a single JPEG.
+ * \param dataSize          Size of the JPEG
+ * \param presentationTime  Stream is running for <presentationTime>
+ */
 void ExtendedFileSink::addData(unsigned char const* data, unsigned dataSize, struct timeval presentationTime) {
     if (data != NULL) {
         mongo::ScopedDbConnection c("localhost");
@@ -39,12 +73,18 @@ void ExtendedFileSink::addData(unsigned char const* data, unsigned dataSize, str
         ss << presentationTime.tv_usec << this->stringfNameSuffix;
         std::cout <<"addData:" << this->stringfNameSuffix<< std::endl;
 
-        gfs.storeFile(reinterpret_cast<const char*>(data), dataSize, this->stringfNameSuffix);//ss.str());
+        gfs.storeFile(reinterpret_cast<const char*>(data), dataSize, this->stringfNameSuffix);
 
         c.done();
     }
 }
 
+/*!
+ * \brief Callback function which is called, as soon as a frame is received
+ * \param frameSize             The data that has to be written. Usually, it is a single JPEG.
+ * \param numTruncatedBytes     Size of the JPEG
+ * \param presentationTime      Stream is running for <presentationTime>
+ */
 void ExtendedFileSink::afterGettingFrame(unsigned frameSize, unsigned numTruncatedBytes, struct timeval presentationTime) {
     if (numTruncatedBytes > 0) {
         envir() << "ExtendedFileSink::afterGettingFrame(): The input frame data was too large for our buffer size ("
--- extendedfilesink.h
+++ extendedfilesink.h
@@ -8,13 +8,16 @@
 #include "mongo/client/dbclient.h"
 #include "mongo/client/dbclientcursor.h"
 
+/*!
+ * \brief The ExtendedFileSink class saves JPEGs to a MongoDB
+ */
 class ExtendedFileSink: public MediaSink {
 public:
     static ExtendedFileSink* createNew(UsageEnvironment& env, std::string tmpCamNameSuffix, unsigned bufferSize = 20000);
     void addData(unsigned char const* data, unsigned dataSize, struct timeval presentationTime);
 
 protected:
-    ExtendedFileSink(UsageEnvironment& env, FILE* fid, unsigned bufferSize, std::string tmpCamNameSuffix);
+    ExtendedFileSink(UsageEnvironment& env, unsigned bufferSize, std::string tmpCamNameSuffix);
       // called only by createNew()
     virtual ~ExtendedFileSink();
 
@@ -26,9 +29,11 @@ protected:
     static void afterGettingFrame(void* clientData, unsigned frameSize, unsigned numTruncatedBytes, struct timeval presentationTime, unsigned durationInMicroseconds);
     virtual void afterGettingFrame(unsigned frameSize, unsigned numTruncatedBytes, struct timeval presentationTime);
 
-    FILE* fOutFid;
+    /// Buffer for receiving a JPEG
     unsigned char* fBuffer;
+    /// Size of fBuffer
     unsigned fBufferSize;
+    /// Name under which the file will be saved
     std::string stringfNameSuffix;
 };
 
